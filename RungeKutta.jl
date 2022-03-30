5272df8e2be26ceb88f686543237cb1a9c7fa5e8
GhostAkr <ighost99@gmail.com>
2022-03-30 18:17:00 +0300

    [+] ODE solver with Runge - Kutta method. - Added method which allows to solve ODE with Runge - Kutta algorithm. - Added calculation of 'max' norm of a vector.

--- RungeKutta.jl
+++ RungeKutta.jl
@@ -70,3 +70,128 @@ function make_step_rk(f::Function, t::Real, prev_val::Vector{Real}, curr_step::R
 
     return prev_val + subsum
 end  # make_step_rk
+
+"""
+    maxnorm(vec::Vector{Real})
+
+Computes 'max' norm of given vector `vec`. Max norm can be calculated as follows:
+``\\underset{i = 1, \\dots n}{\\text{max}} |v_i|``. Return norm of `vec` as one real
+number.
+
+# Arguments
+`vec::Vector{Real}`: vector which norm should be calculated.
+"""
+function maxnorm(vec::Vector{Real})
+    if isempty(vec)
+        @error("Trying to calculate norm of empty vector")
+    end
+
+    max_component = abs(vec[1])
+    for component in vec
+        abs_comp = abs(component)
+
+        if abs_comp > max_component
+            max_component = abs_comp
+        end
+    end
+
+    return max_component
+end  # maxnorm
+
+"""
+	solve_rk(f::Function, t_limits::Tuple{Real}, initial_step::Real, 
+		initial_vals::Tuple{Real}, max_stage::Integer, c_coeffs::Tuple{Real}, 
+		b_coeffs::Tuple{Real}, a_coeffs::Tuple{Tuple{Real}})
+
+Solve system of ODEs using Runge - Kutta method. System of ODE can be represented as
+follows: ``y' = f(t, y), y(t_0) = y_0`` where y is a vector. To achieve more accurancy
+this method uses Richardson's extrapolation to control step during the computational 
+process.
+
+Return dictionary in following format: `<t>: <y(t)`.
+
+Function `f` takes ``t`` and vector ``y`` and returns vector of real values which length
+is equal to the length of ``y``.
+
+``c`` and ``b`` coefficients are stored as usual tuples where each element of tuple
+denotes coefficient for appropriate stage. ``a`` coefficients are represented as nested 
+tuples. Each internal tuple contains coeffcients of one method's stage. For example for 
+4-staged method we consider following structure of ``a`` coefficients:
+((a21), (a31, a32), (a41, a42, a43)). Length of tuples with coefficients should 
+correspond to `max_stage` value.
+
+# Arguments
+- `f::Function`: right part of the system of ODEs;
+- `t_limits::Tuple{Real}`: system argument limits;
+- `initial_step::Real`: initial step;
+- `initial_val::Tuple{Real}`: initial values (``y_0``);
+- `tol::Real`: target tolerance;
+- `max_stage::Integer`: total number of stages;
+- `c_coeffs::Tuple{Real}`: tuple with``c`` coefficients of the method;
+- `b_coeffs::Tuple{Real}`: tuple with ``b`` coefficients of the method;
+- `a_coeffs::Tuple{Tuple{Real}}`: ``a`` coefficients of the method.
+"""
+function solve_rk(f::Function, t_limits::Tuple{Real}, initial_step::Real, 
+	initial_val::Tuple{Real}, tol::Real, max_stage::Integer, c_coeffs::Tuple{Real},
+	b_coeffs::Tuple{Real}, a_coeffs::Tuple{Tuple{Real}}
+)
+    curr_val = initial_val
+    curr_step = initial_step
+    t = t_limits[1]  # Current argument value
+
+    solution = Dict{Real, Real}()
+
+    while t < t_limits[2]
+        near_end = false
+        if t + curr_step * 2 > t_limits[2]
+            near_end = true
+        end
+
+        if near_end
+            curr_step = (t_limits[2] - t) / 2
+        end
+
+
+        while (true)
+            # Make 2 usual steps
+            y1 = make_step_rk(f, t, curr_val, curr_step, max_stage, c_coeffs, b_coeffs, 
+                a_coeffs)
+            y2 = make_step_rk(f, t + curr_step, y1, curr_step, max_stage, c_coeffs, 
+                b_coeffs, a_coeffs)
+                
+            # If we are near the end of interval then we don't need to perform Richardson's 
+            # extrapolation
+            if (near_end)
+                solution[t + curr_step] = y1
+                solution[t + curr_step * 2] = y2
+                return solution
+            end
+
+            # Make 1 double step
+            w = make_step_rk(f, t, curr_val, curr_step * 2, max_stage, c_coeffs, b_coeffs, 
+                a_coeffs)
+
+            err = 1 / (2^p - 1) * maxnorm(y2 - w)
+
+            fac = 0.9
+            facmin = 0.5
+            facmax = 3
+            new_step = curr_step * min(facmax, max(facmin, fac * 
+                (tol / err)^(1 / (max_stage))))
+
+            if err <= tol
+                t += (curr_step * 2)
+                curr_val = y2
+                curr_step = new_step
+                break
+            else
+                curr_step = new_step
+            end
+        end
+
+        solution[t - curr_step] = y1
+        solution[t] = y2
+    end
+
+    return solution
+end  # solve_rk
