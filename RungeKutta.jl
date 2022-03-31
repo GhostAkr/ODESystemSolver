2db86fa0a41831f3cb08679f37eb7ae28d77f5d8
GhostAkr <ighost99@gmail.com>
2022-03-31 14:29:22 +0300

    [+] Nested Runge - Kutta solver.

--- RungeKutta.jl
+++ RungeKutta.jl
@@ -160,7 +160,7 @@ function solve_rk(f::Function, t_limits::Tuple{Float64, Float64}, initial_step::
         y1 = 0.
         y2 = 0.
 
-        while (true)
+        while true
             # Make 2 usual steps
             y1 = make_step_rk(f, t, curr_val, curr_step, max_stage, c_coeffs, b_coeffs, 
                 a_coeffs)
@@ -169,7 +169,7 @@ function solve_rk(f::Function, t_limits::Tuple{Float64, Float64}, initial_step::
                 
             # If we are near the end of interval then we don't need to perform Richardson's 
             # extrapolation
-            if (near_end)
+            if near_end
                 solution[t + curr_step] = y1
                 solution[t + curr_step * 2] = y2
                 return solution
@@ -203,3 +203,98 @@ function solve_rk(f::Function, t_limits::Tuple{Float64, Float64}, initial_step::
 
     return solution
 end  # solve_rk
+
+"""
+	solve_nested_rk(f::Function, t_limits::Tuple{Real}, initial_step::Real,
+		initial_val::Vector{Real}, tol::Real, max_stage::Integer, c_coeffs::Vector{Real},
+		b_coeffs::Vector{Real}, bhat_coeffs::Vector{Real},  a_coeffs::Vector{Vector{Real}})
+
+Solve system of ODEs using nested Runge - Kutta method. System of ODE can be represented
+as follows: ``y' = f(t, y), y(t_0) = y_0`` where y is a vector. Nested Runge - Kutta 
+method makes 2 calculations at one step. Then it adapts step according to difference
+between these 2 values.
+
+Function `f` takes ``t`` and vector ``y`` and returns vector of real values which length
+is equal to the length of ``y``.
+
+``c``, ``b`` and ``\\hat{b}`` coefficients are stored as usual vectors where each element
+of vector denotes coefficient for appropriate stage. ``a`` coefficients are represented 
+as nested vectors. Each internal vector contains coeffcients of one method's stage. 
+For example for 4-staged method we consider following structure of ``a`` coefficients:
+[[a21], [a31, a32], [a41, a42, a43]]. Length of tuples with coefficients should 
+correspond to `max_stage` value.
+
+# Arguments
+- `f::Function`: right part of the system of ODEs;
+- `t_limits::Tuple{Float64, Float64}`: system argument limits;
+- `initial_step::Float64`: initial step;
+- `initial_val::Vector{Float64}`: initial values (``y_0``);
+- `tol::Float64`: target tolerance;
+- `max_stage::Integer`: total number of stages;
+- `c_coeffs::Vector{Float64}`: tuple with``c`` coefficients of the method;
+- `b_coeffs::Vector{Float64}`: tuple with ``b`` coefficients of the method;
+- `bhat_coeffs::Vector{Float64}`: tuple with ``\\hat{b}`` coefficients of the method;
+- `a_coeffs::Vector{Vector{Float64}}`: ``a`` coefficients of the method.
+"""
+function solve_nested_rk(f::Function, t_limits::Tuple{Real}, initial_step::Real,
+	initial_val::Vector{Real}, tol::Real, max_stage::Integer, c_coeffs::Vector{Real},
+	b_coeffs::Vector{Real}, bhat_coeffs::Vector{Real},  a_coeffs::Vector{Vector{Real}}
+)
+    curr_val = initial_val
+    curr_step = initial_step
+    t = t_limits[1]
+
+    solution = Dict{Float64, Vector{float64}}()
+
+    solution[t] = curr_val
+
+    while t < t_limits[2]
+        near_end = false
+        if t + curr_step > t_limits[2]
+            near_end = true
+        end
+
+        if near_end
+            curr_step = t_limits[2] - t
+        end
+
+        y1 = 0.
+
+        while true
+            # Main step
+            y1 = make_step_rk(f, t, curr_val, curr_step, max_stage, c_coeffs, b_coeffs, 
+                a_coeffs)
+
+            if near_end
+                solution[t + curr_step] = y1
+                return solution
+            end
+
+            # Step for accuracy control
+            y1hat = make_step_rk(f, t, curr_val, curr_step, max_stage, c_coeffs, 
+                bhat_coeffs, a_coeffs)
+
+            err = maxnorm(y1 - y1hat)
+
+            fac = 0.9
+            facmin = 0.5
+            facmax = 3
+            new_step = curr_step * min(facmax, max(facmin, fac * 
+                (tol / err)^(1 / (max_stage))))
+
+            if err <= tol
+                t += curr_step
+                curr_val = y1
+                curr_step = new_step
+                break
+            else
+                curr_step = new_step
+                facmax = 1
+            end
+        end
+
+        solution[t] = y1
+    end
+
+    return solution
+end  # solve_nested_rk
