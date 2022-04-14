6be8767bb12019744c6ee5a534b235851b950ee7
GhostAkr <ighost99@gmail.com>
2022-04-14 19:06:37 +0300

    [+] Output results of nested RK solver to file.

--- Main.jl
+++ Main.jl
@@ -216,6 +216,128 @@ function output_results_rk()
     output_by_argument_rk(FacMin)
 end  # output_results_rk
 
+"""
+    output_by_argument_dp()
+
+Output Dormand - Prince calculation results as a tables. Argument of each table is `arg`. 
+Table values: total count of steps, count of rejected steps, total calculation time, global 
+error.
+
+# Arguments
+`arg::ResultArgument`: parameter which should be the argument of tables.
+"""
+function output_by_argument_dp(arg::ResultArgument)
+    t_limits = (0.1, 4.1)
+    initial_steps = [step for step in 0.01:0.001:0.2]
+    initial_val = exact_sol(t_limits[1])
+    tol = 1e-4
+    max_stage = 4
+    coeffs = coeffs_dp()
+    facs = [fac for fac in 0.8:0.001:0.9]
+    facmins = [facmin for facmin in 0.2:0.001:0.66]
+    facmaxes = [facmax for facmax in 1.5:0.01:5]
+
+    respath = "results"
+    mkpath(respath)
+
+    total_steps_data = []
+    rejected_steps_data = []
+    total_time_data = []
+    global_err_data = []
+
+    param_vec = []
+    file_prefix = ""
+    if arg == InitStep
+        file_prefix = "initstep"
+        param_vec = initial_steps
+    elseif arg == Fac
+        file_prefix = "fac"
+        param_vec = facs
+    elseif arg == FacMax
+        file_prefix = "facmax"
+        param_vec = facmaxes
+    elseif arg == FacMin
+        file_prefix = "facmin"
+        param_vec = facmins
+    end
+
+    for param in param_vec
+        res_tuple = Tuple
+
+        if arg == InitStep
+            res_tuple = solve_nested_rk(f, t_limits, param, initial_val, tol, max_stage, 
+                coeffs[1], coeffs[2], coeffs[3], coeffs[4], facs[1], facmins[1], 
+                facmaxes[1])
+        elseif arg == Fac
+            res_tuple = solve_nested_rk(f, t_limits, initial_steps[1], initial_val, tol, max_stage, 
+                coeffs[1], coeffs[2], coeffs[3], coeffs[4], param, facmins[1], facmaxes[1])
+        elseif arg == FacMax
+            res_tuple = solve_nested_rk(f, t_limits, initial_steps[1], initial_val, tol, max_stage, 
+                coeffs[1], coeffs[2], coeffs[3], coeffs[4], facs[1], facmins[1], param)
+        elseif arg == FacMin
+            res_tuple = solve_nested_rk(f, t_limits, initial_steps[1], initial_val, tol, max_stage, 
+                coeffs[1], coeffs[2], coeffs[3], coeffs[4], facs[1], param, facmaxes[1])
+        end
+
+        num_sol = res_tuple[1]
+        total_steps = res_tuple[2]
+        rejected_steps = res_tuple[3]
+        total_time = res_tuple[4]
+
+        err_vec = Vector{Real}()
+        for (t, y) in num_sol
+            exact_val = exact_sol(t)
+            append!(err_vec, maxnorm(exact_val - y))
+        end
+
+        push!(total_steps_data, [param, total_steps])
+        push!(rejected_steps_data, [param, rejected_steps])
+        push!(total_time_data, [param, total_time])
+        push!(global_err_data, [param, max(err_vec...)])
+    end
+
+    file_name = respath * "/" * file_prefix * "_n.dat"
+    open(file_name, "w") do outfile
+        writedlm(outfile, total_steps_data)
+    end
+
+    file_name = respath * "/" * file_prefix * "_nrej.dat"
+    open(file_name, "w") do outfile
+        writedlm(outfile, rejected_steps_data)
+    end
+
+    file_name = respath * "/" * file_prefix * "_time.dat"
+    open(file_name, "w") do outfile
+        writedlm(outfile, total_time_data)
+    end
+
+    file_name = respath * "/" * file_prefix * "_err.dat"
+    open(file_name, "w") do outfile
+        writedlm(outfile, global_err_data)
+    end
+end  # output_by_argument_rk
+
+"""
+    output_results_dp()
+
+Perform calculations with Dormand - Prince method and output results to external files.
+"""
+function output_results_dp()
+
+    @info "Solving for different initial steps..."
+    output_by_argument_dp(InitStep)
+    
+    @info "Solving for different facs..."
+    output_by_argument_dp(Fac)
+
+    @info "Solving for different facmaxes..."
+    output_by_argument_dp(FacMax)
+
+    @info "Solving for different facmins..."
+    output_by_argument_dp(FacMin)
+end  # output_results_rk
+
 # test_rk()
-test_dp()
+# test_dp()
 # output_results_rk()
+output_results_dp()
